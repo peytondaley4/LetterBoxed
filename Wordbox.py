efa7e9d97a2deb88ba9b9498f532f52cd8ff1eb4
peytondaley4 <95379090+peytondaley4@users.noreply.github.com>
2024-09-24 13:22:47 -0400

    Working solution
    Changed from recursive search with pruning to brute forcing, small solution space makes this run in less than a second.

--- Wordbox.py
+++ Wordbox.py
@@ -1,10 +1,20 @@
-from english_words import get_english_words_set
-wordset = get_english_words_set(['web2'], alpha=True, lower=True)
+with open('words_alpha.txt') as word_file:
+    wordset = set(word_file.read().split())
+
 trunkset = set()
 finalset = set()
 
-box = [{'u', 'h', 'k'}, {'a', 'l', 'g'}, {'p', 'd', 'r'}, {'s', 'o', 'n'}]
-boxset = {'u', 'h', 'k', 'a', 'l', 'g', 'p', 'd', 'r', 's', 'o', 'n'}
+box = []
+for i in range(4):
+	box.append(set())
+	string = input("Provide line: ")
+	for c in string:
+		box[i].add(c)
+
+boxset = set()
+for b in box:
+	for e in b:
+		boxset.add(e)
 
 if __name__ == "__main__":
 	for word in wordset:
@@ -14,8 +24,6 @@ if __name__ == "__main__":
 				test = True
 		if not test and len(word) > 2:
 			trunkset.add(word)
-
-	print(len(trunkset))
 
 	for word in trunkset:
 		test = False
@@ -27,5 +35,39 @@ if __name__ == "__main__":
 				finalset.add(word)
 
 	sortlist = sorted(finalset, key=len)[::-1]
-	print(len(sortlist))
-	print(sortlist)
+	
+	test = False
+
+	for i in range(len(sortlist)):
+		if len(set(sortlist[i])) == 12:
+			print("In 1: ", sortlist[i])
+			test = True
+	
+	if test:
+		quit()
+
+	for i in range(len(sortlist)-1):
+		for j in range(i+1, len(sortlist)):
+			if sortlist[i][-1] == sortlist[j][0]:
+				if len(set(sortlist[i]).union(set(sortlist[j]))) == 12:
+					print("In 2: ", sortlist[i], sortlist[j])
+					if len(sortlist[i] + sortlist[j]) == 13:
+						print("No Repeats^")
+					test = True
+
+	if test:
+		quit()
+
+	for i in range(len(sortlist)-2):
+		for j in range(i+1, len(sortlist)-1):
+			if sortlist[i][-1] == sortlist[j][0]:
+				for k in range(j+1, len(sortlist)):
+					if sortlist[j][-1] == sortlist[k][0]:
+						if len(set(sortlist[i]).union(set(sortlist[j]).union(set(sortlist[k])))) == 12:
+							print("in 3: ", sortlist[i], sortlist[j], sortlist[k])
+							test = True
+
+	if test:
+		quit()
+
+	print("No 3-word or less solution found")
